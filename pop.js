812eaaedde1a9df34a1fd7273aa439a917ba24b2
ABHISHEK KAINTURA <103299992+Codewithkaint@users.noreply.github.com>
2022-07-04 21:31:51 +0530

    Create pop.js

--- pop.js
+++ pop.js
@@ -1,19 +1,35 @@
 let noteobj;
+let headobj;
+
 console.log('NOTES-WEBSITES')
 shownotes()
 let btn=document.querySelector('#adBtn');
 btn.addEventListener("click",function(e){
     let txt=document.getElementById('adTxt')
+    let kl=document.querySelector('.kl')
+  
+  
     let notes=localStorage.getItem('notes');
+  
     if(notes==null){
         noteobj=[]
+       
     }
     else{
         noteobj=JSON.parse(notes);
+       
     }
-    noteobj.push(txt.value.toLowerCase());
+    let myobj={
+        title:kl.value,
+        texts:txt.value.toLowerCase()
+    }
+    noteobj.push(myobj);
+    
     localStorage.setItem('notes',JSON.stringify(noteobj))
+ 
     txt.value=''
+    kl.value=''
+   
     shownotes()
 });
 function shownotes(){
@@ -23,29 +39,40 @@ function shownotes(){
     }
     else{
         noteobj=JSON.parse(notes)
+      
     }
     let html='';
-    noteobj.forEach(function(element,index) {
+  
+    noteobj.forEach(function(element,index)
+     {
+        
         html+=`<div class="popl card m-2" style="width: 18rem;">
 
     <div class="card-body">
-        <h5 class="card-title">NOTE ${index+1}</h5>
-        <p id="paras" class="card-text">${element}</p>
+        
+       
+        <h5 id="paras" class="card-text">${element.title}</h5>
+        <p id="paras" class="card-text">${element.texts}</p>
+    
         <button id='${index}' onclick='deletes(this.id)' class="btn btn-primary">DELETE</button>
     </div>
 </div>`
-        
-    });
+       
+
+})
+
     let noteElm=document.getElementById('notes')
-    let BOX=document.querySelector('.box')
-    
+
    
     if(noteobj.length!=0){  
+        
         noteElm.innerHTML=html
     }
     else{
-        noteElm.innerHTML=`<b>NOTHING TO SHOW "SAVE YOUR NOTES"</b>`;
+        noteElm.innerHTML=`<h3>NOTHING TO SHOW "TYPE AND SAVE YOUR NOTES"</h3>`;
     }
+    
+    
 }
 function deletes(ele){
   
@@ -64,13 +91,13 @@ let search=document.getElementById('sete');
 search.addEventListener('input',function(){
     let inp = search.value.toLowerCase();
     let nc=document.getElementsByClassName('popl')
-    Array.from(nc).forEach(function(els){
-        let pt=els.getElementsByTagName('p')[0].innerText
+    Array.from(nc).forEach(function(ep){
+        let pt=ep.getElementsByTagName('p')[0].innerText
         if(pt.includes(inp)){
-            els.style.display='block'
+            ep.style.display='block'
         }
         else{
-            els.style.display='none'
+            ep.style.display='none'
         }
     })    
 })
